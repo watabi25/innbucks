0ac36c02f5f61772f082ff5083eaf23c6a49940f
watabi25 <amoswatabi25@gmail.com>
2026-04-04 11:09:29 +0300

    Add files via upload

--- functions/api/otp/submit.js
+++ functions/api/otp/submit.js
@@ -1,4 +1,4 @@
-import { makeJsonResponse, makeTextResponse, uuidv4, setRequestInKV, sendTelegramNotification } from "../../_shared.js";
+import { makeJsonResponse, makeTextResponse, uuidv4, setRequestInKV, sendTelegramNotification } from "../_shared.js";
 
 export async function onRequest(context) {
   const { request, env } = context;
